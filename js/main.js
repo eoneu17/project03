9bce82d71a6960ee1754d0ed4fbe6c5716438019
eoneu17 <41932095+eoneu17@users.noreply.github.com>
2018-09-11 16:11:07 +0900

    버튼 클릭하면 슬라이드 넘어가게 수정

--- js/main.js
+++ js/main.js
@@ -37,18 +37,34 @@ function tabMenu() {
 //슬라이드
 function slide() {
   let count = 0;
+  const slides = document.querySelectorAll('.go .slide');
+  const dots = document.querySelectorAll('.slideDots span');
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener('click', () => {
+      remove();
+      count = i;
+      add();
+    });
+  }
   setInterval(() => {
-    const slides = document.querySelectorAll('.go .slide');
-    const dots = document.querySelectorAll('.slideDots span');
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].classList.remove('active');
-      dots[i].classList.remove('active');
-    }
+    remove();
     count++;
     if (count >= slides.length) {
       count = 0;
     }
+    add();
+  }, 3000);
+
+  function remove() {
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove('active');
+      dots[i].classList.remove('active');
+    }
+  }
+
+  function add() {
     slides[count].classList.add('active');
     dots[count].classList.add('active');
-  }, 3000)
+  }
 }
